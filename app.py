4de25bac12077dba00d7dbdb05f52e833fcc958f
Payalsasmal10 <sasmalpayal@gmail.com>
2020-05-07 16:39:30 +0530

    added port no

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-
 from flask import Flask
 
 app = Flask(__name__)
@@ -6,4 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
 
-    return 'Hello World! I am a customize image'
+    return 'Hello guys! I am a customize image'
+    
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=5000)
